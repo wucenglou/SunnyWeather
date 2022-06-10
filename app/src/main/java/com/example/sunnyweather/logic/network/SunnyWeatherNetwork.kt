75a72ded1b5f62acf74caae9c762ceda52b83d2d
Rowen <wucenglou@qq.com>
2022-06-10 18:16:57 +0800

    second commit

--- app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
@@ -6,9 +6,15 @@ import retrofit2.Response
 import kotlin.coroutines.resumeWithException
 import kotlin.coroutines.suspendCoroutine
 
+/**
+ * 网络层
+ */
 object SunnyWeatherNetwork {
     private val placeService = ServiceCreator.create<PlaceService>()
 
+    /**
+     * 当外部调用SunnyWeatherNetwork的searchPlaces()函数时，Retrofit就会立即发起网络请求，同时当前的协程也会被阻塞住
+     */
     suspend fun searchPlaces(query: String) = placeService.searchCity(query).await()
 
     private suspend fun <T> Call<T>.await(): T {
