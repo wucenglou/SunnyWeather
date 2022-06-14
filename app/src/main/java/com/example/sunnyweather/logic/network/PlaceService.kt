9147c1ef1b3607967a01d95f60bd1ca8efc26841
Rowen <wucenglou@qq.com>
2022-06-14 18:18:16 +0800

    6.14 commit

--- app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
@@ -1,9 +1,12 @@
 package com.example.sunnyweather.logic.network
 
 import com.example.sunnyweather.SunnyWeatherApplication
+import com.example.sunnyweather.logic.model.DailyResponse
 import com.example.sunnyweather.logic.model.PlaceResponse
+import com.example.sunnyweather.logic.model.RealtimeResponse
 import retrofit2.Call
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface PlaceService {
@@ -16,6 +19,13 @@ interface PlaceService {
      */
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
+
+    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
+    fun getRealtimeWeather(@Path("lng") lng: String,@Path("lat") lat: String): Call<RealtimeResponse>
+
+    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/daily.json")
+    fun getDailyWeather(@Path("lng") lng: String,@Path("lat") lat: String): Call<DailyResponse>
+
 
     /**
      * 免费api
