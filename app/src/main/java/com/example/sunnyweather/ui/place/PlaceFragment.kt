e070348e47d54bba4bf929bed707f5e74d82e475
Rowen <wucenglou@qq.com>
2022-06-14 16:53:21 +0800

    6.14 commit

--- app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
+++ app/src/main/java/com/example/sunnyweather/ui/place/PlaceFragment.kt
@@ -20,26 +20,8 @@ import com.example.sunnyweather.R
 class PlaceFragment : Fragment() {
     //延迟初始化viewModel对象
     private val viewModel by lazy { ViewModelProvider(this).get(PlaceViewModel::class.java) }
-//    internal val viewModel = viewModels<LocationViewModel> {
-//        ViewModelProvider.NewInstanceFactory()
-//    }
     //延迟初始化recyclerView适配器
     private lateinit var adapter: PlaceAdapter
-//
-//    private lateinit var recyclerView: RecyclerView
-//    private lateinit var actionBarLayout: FrameLayout
-//    private lateinit var searchPlaceEdit: EditText
-//    private lateinit var bgImageView: ImageView
-//    private lateinit var mRootView: View
-
-    //控件绑定
-//    private fun initViews(view: View) {
-//        bgImageView = view.findViewById(R.id.bgImageView)
-//        searchPlaceEdit = view.findViewById(R.id.searchPlaceEdit)
-//        actionBarLayout = view.findViewById(R.id.actionBarLayout)
-//        recyclerView = view.findViewById(R.id.recyclerView)
-//    }
-
     //绑定布局资源
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -48,7 +30,6 @@ class PlaceFragment : Fragment() {
     ): View? {
         return inflater.inflate(R.layout.fragment_place, container, false)
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
@@ -78,57 +59,11 @@ class PlaceFragment : Fragment() {
                 bgImageView.visibility = View.GONE
                 viewModel.placeList.clear()
                 viewModel.placeList.addAll(places)
-//                viewModel.placeList.addAll(places)
                 adapter.notifyDataSetChanged()
             } else {
                 Toast.makeText(activity, "未能查到任何地点", Toast.LENGTH_SHORT).show()
                 result.exceptionOrNull()?.printStackTrace()
             }
         })
-
     }
-
-//    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-//        super.onViewCreated(view, savedInstanceState)
-        //如果当前Fragment属于MainActivity并且已有存储的城市数据,
-        // 则获取并解析成Location对象，设置参数并跳转天气界面
-        // (防止被天气界面嵌入导致无限循环跳转页面)
-//        if (activity is MainActivity && viewModel.value.isPlace)
-//    }
-
-//    override fun onActivityCreated(savedInstanceState: Bundle?) {
-//        super.onActivityCreated(savedInstanceState)
-//
-//        val recyclerView: RecyclerView = view.findViewById(R.id.recyclerView)
-//        val searchPlaceEdit: EditText = findViewById(R.id.searchPlaceEdit)
-//        val bgImageView: ImageView =findViewById(R.id.bgImageView)
-//        val layoutManager = LinearLayoutManager(activity)
-//        recyclerView.layoutManager = layoutManager
-//        adapter = PlaceAdapter(this,viewModel.placeList)
-//        recyclerView.adapter = adapter
-//        searchPlaceEdit.addTextChangedListener { editable ->
-//            val content = editable.toString()
-//            if (content.isNotEmpty()){
-//                viewModel.searchPlaces(content)
-//            } else {
-//                recyclerView.visibility = View.GONE
-//                bgImageView.visibility = View.VISIBLE
-//                viewModel.placeList.clear()
-//                adapter.notifyDataSetChanged()
-//            }
-//        }
-//        viewModel.placeLiveData.observe(viewLifecycleOwner, Observer { result ->
-//            val places = result.getOrNull()
-//            if (places != null) {
-//                recyclerView.visibility = View.VISIBLE
-//                bgImageView.visibility = View.GONE
-//                viewModel.placeList.clear()
-//                viewModel.placeList.addAll(places)
-//                adapter.notifyDataSetChanged()
-//            } else {
-//                Toast.makeText(activity, "未能查到任何地点", Toast.LENGTH_SHORT).show()
-//                result.exceptionOrNull()?.printStackTrace()
-//            }
-//        })
-//    }
 }
