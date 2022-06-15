5e1c8e4fdef919d19f7d261ad1e61b668f649d04
Rowen <wucenglou@qq.com>
2022-06-15 17:00:35 +0800

    6.15 commit

--- app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/PlaceService.kt
@@ -20,13 +20,6 @@ interface PlaceService {
     @GET("v2/place?token=${SunnyWeatherApplication.TOKEN}&lang=zh_CN")
     fun searchPlaces(@Query("query") query: String): Call<PlaceResponse>
 
-    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/realtime.json")
-    fun getRealtimeWeather(@Path("lng") lng: String,@Path("lat") lat: String): Call<RealtimeResponse>
-
-    @GET("v2.5/${SunnyWeatherApplication.TOKEN}/{lng},{lat}/daily.json")
-    fun getDailyWeather(@Path("lng") lng: String,@Path("lat") lat: String): Call<DailyResponse>
-
-
     /**
      * 免费api
      */
--- app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
+++ app/src/main/java/com/example/sunnyweather/logic/network/SunnyWeatherNetwork.kt
@@ -12,11 +12,15 @@ import kotlin.coroutines.suspendCoroutine
  */
 object SunnyWeatherNetwork {
     private val placeService = ServiceCreator.create<PlaceService>()
+    private val weatherService = ServiceCreator.create(WeatherService::class.java)
 
     /**
      * 当外部调用SunnyWeatherNetwork的searchPlaces()函数时，Retrofit就会立即发起网络请求，同时当前的协程也会被阻塞住
      */
     suspend fun searchPlaces(query: String) = placeService.searchPlaces(query).await()
+
+    suspend fun getDailyWeather(lng: String, lat: String) = weatherService.getDailyWeather(lng,lat).await()
+    suspend fun getRealtimeWeather(lng: String,lat: String) = weatherService.getRealtimeWeather(lng, lat).await()
 
     private suspend fun <T> Call<T>.await(): T {
         return suspendCoroutine { continuation ->
